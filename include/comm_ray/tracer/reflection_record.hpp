1ff20f2fdd1672a08a96260b64d65223fa2f9f41
hieule <hieu.tg.le@gmail.com>
2023-06-27 17:54:44 +0700

    reflection record

---
+++ include/comm_ray/tracer/reflection_record.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#ifndef REFLECTION_RECORD_HPP
+#define REFLECTION_RECORD_HPP
+
+#include "glm/glm.hpp"
+#include <iostream>
+
+namespace signal_tracer {
+
+    struct ReflectionRecord {
+        int reflection_count{};
+        std::vector<glm::vec3> ref_points{};
+
+        // cout overload
+        friend std::ostream& operator<<(std::ostream& os, const ReflectionRecord& record) {
+            os << "ReflectionRecord: " << std::endl;
+            os << "Reflection count: " << record.reflection_count << std::endl;
+            os << "Trace path: " << std::endl;
+            for (const auto& point : record.ref_points) {
+                os << "\t" << glm::to_string(point) << std::endl;
+            }
+            return os;
+        }
+    };
+}
+
+#endif // !REFLECTION_RECORD_HPP
