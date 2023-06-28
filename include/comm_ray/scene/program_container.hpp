0935d75c38078655e73f69ac8a9651ca5a61e71e
hieule <hieu.tg.le@gmail.com>
2023-06-28 23:31:55 +0700

    add container for shader program wrapper

---
+++ include/comm_ray/scene/program_container.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#ifndef PROGRAM_CONTAINER_HPP
+#define PROGRAM_CONTAINER_HPP
+
+#include "glad/gl.h"
+#include "glm/glm.hpp"
+#include "cyGL.h"
+#include "drawable.hpp"
+#include "lighting.hpp"
+#include <string>
+#include <vector>
+#include <memory>
+
+namespace signal_tracer {
+
+    struct WindowParams {
+        int width{};
+        int height{};
+        std::string title{};
+        bool debug_flag{ false };
+    };
+
+    struct ShaderProgram {
+        // A thin wrapper for cy::GLSLProgram
+        std::string name;
+        cy::GLSLProgram program;
+        std::vector<std::shared_ptr<Light>> lights;
+        std::vector<std::shared_ptr<Drawable>> drawables;
+        glm::vec3 view_mat{ 1.0f };
+        glm::vec3 proj_mat{ 1.0f };
+    };
+}
+
+#endif // !PROGRAM_CONTAINER_HPP
