a6baeab6a6af0da0f7b9cf9fe465311a72d2e867
hieule <hieu.tg.le@gmail.com>
2023-06-29 18:52:14 +0700

    refactor code

--- apps/app.cpp
+++ apps/app.cpp
@@ -2,22 +2,16 @@
 #include <GLFW/glfw3.h>
 
 #include "config.hpp"
-#include "orbit_camera.hpp"
-
-#include "utils.hpp"
-#include "model.hpp"
-#include "lighting.hpp"
 #include "cyGL.h"
 #include "cubesphere.h"
-#include "signal_tracer.hpp"
-#include "ray.hpp"
+#include "lighting.hpp"
 #include "line.hpp"
-#include "point_container.hpp"
-#include "triangle_draw.hpp"
-#include "viewing.hpp"
-#include "triangle.hpp"
-#include "scene.hpp"
+#include "model.hpp"
+#include "orbit_camera.hpp"
 #include "program_container.hpp"
+#include "signal_tracer.hpp"
+#include "scene.hpp"
+#include "viewing.hpp"
 
 #include "glm/gtc/type_ptr.hpp"
 #include "glm/gtc/matrix_transform.hpp"
@@ -73,97 +67,92 @@ int main(int argc, char* argv []) {
     cy::GLSLProgram radio_prog{};
     if (radio_prog.BuildFiles("/home/hieule/research/comm_ray/shader/sphere.vert", "/home/hieule/research/comm_ray/shader/sphere.frag")) {}
 
-
     // Load models
     // ----------------------------------
     auto city_model_ptr{ std::make_shared<signal_tracer::Model>("/home/hieule/research/comm_ray/assets/demo_layouts/Basic_Demo/OBJ/Basic_Demo_Layout_OBJ.obj") };
 
-    auto city_model_ptr2{ std::make_shared<signal_tracer::Model>("/home/hieule/research/comm_ray/assets/demo_layouts/Basic_Demo/OBJ/Basic_Demo_Layout_OBJ.obj") };
-    city_model_ptr2->transform(glm::translate(glm::vec3{ 0.0f, 0.0f, -15.0f }));
-
-    std::vector<std::shared_ptr<signal_tracer::Model>> model_ptrs{ city_model_ptr };
-    // Model city_model_ptr{ "/home/hieule/research/comm_ray/assets/demo_layouts/City_Demo/OBJ/City_Map_Layout_OBJ.obj" };
-
+    // auto city_model_ptr{ std::make_shared<signal_tracer::Model>("/home/hieule/research/comm_ray/assets/demo_layouts/City_Demo/OBJ/City_Map_Layout_OBJ.obj") };
 
     // Lighting
     // ----------------------------------
-    auto directional_light_ptr{ std::make_shared<signal_tracer::DirectionalLight>(
-        glm::vec3(0.0f, -1.0f, 0.0f), // direction
-        glm::vec3(1.0f, 1.0f, 1.0f), // color
-        0.2, // ambient
-        0.5, // diffuse
-        0.5, // specular
-        "directional_light"
-    ) };
-
-    auto flash_light{ std::make_shared<signal_tracer::FlashLight>(
-        glm::vec3(0.0f, 0.0f, 0.0f), // position
-        glm::vec3(0.0f, 0.0f, -1.0f), // direction
-        glm::vec3(1.0f, 1.0f, 1.0f), // color
-        0.1, // ambient
-        0.5, // diffuse
-        0.5, // specular
-        1.0f, // constant
-        0.09f, // linear
-        0.032f, // quadratic
-        glm::cos(glm::radians(10.0f)), // inner cut off
-        glm::cos(glm::radians(30.0f)), // outer cut off
-        "flash_light"
-    ) };
+    auto directional_light_ptr{ std::make_shared<signal_tracer::DirectionalLight>() };
+    auto flash_light{ std::make_shared<signal_tracer::FlashLight>() };
     flash_light->init(city_prog, glm::mat4{ 1.0f });
 
-    // Signal tracer
+    // set drawing for city
     // ----------------------------------
+    std::vector<std::shared_ptr<signal_tracer::Model>> model_ptrs{ city_model_ptr };
+    std::vector<std::shared_ptr<signal_tracer::Drawable>> model_drawable_ptrs(model_ptrs.begin(), model_ptrs.end());
+    std::vector<glm::mat4> model_mats{ };
+    for (std::size_t i = 0; i < model_ptrs.size(); ++i) {
+        model_mats.push_back(glm::mat4{ 1.0f });
+    }
+
+    // Signal tracer
+    glm::vec3 tx_pos{ 5.0f, 2.5f, 5.0f };
+    // glm::vec3 tx_pos{ 0.0f, 2.5f, 20.0f };
+    glm::vec3 rx_pos{ -13.0f, 3.0f, -0.5f };
+
     Utils::Timer timer{};
     timer.reset();
     auto sig_tracer{ std::make_shared<signal_tracer::SignalTracer>(model_ptrs, 2) };
     timer.execution_time();
-
     timer.reset();
-    sig_tracer->tracing(glm::vec3(-10.0f, 3.0f, 5.0f), glm::vec3(-13.0f, 3.0f, -0.5f));
+    sig_tracer->tracing(tx_pos, rx_pos);
     timer.execution_time();
-
-    sig_tracer->init_draw();
-
     scene.set_signal_tracer(sig_tracer);
-
-    // set drawing for radio stations
-    // ----------------------------------
-    auto station_object_ptr{ std::make_shared<signal_tracer::Cubesphere>(0.25f, 3, true) };
-
-    std::vector<glm::mat4> station_model_mats{};
-    for (const auto& pos : sig_tracer->get_station_positions()) {
-        station_model_mats.push_back(glm::translate(glm::mat4{ 1.0f }, pos));
-    }
-
-    // drawing reflections
-    std::vector<std::shared_ptr<signal_tracer::Drawable>> lines{};
-    std::vector<glm::mat4> line_model_mats{};
-    for (auto& line : sig_tracer->get_lines()) {
-        lines.push_back(std::make_shared<signal_tracer::Line>(line));
-        line_model_mats.push_back(glm::mat4{ 1.0f });
-    }
-
-
-    // Shader Programs
-    // ----------------------------------
 
     auto shader_city_prog{ std::make_shared<signal_tracer::ShaderProgram>(
         "city",
         city_prog,
         std::vector<std::shared_ptr<signal_tracer::Light>>{directional_light_ptr},
         std::vector<glm::vec3>{glm::vec3{ 1.0f }},
-        std::vector<std::shared_ptr<signal_tracer::Drawable>>{city_model_ptr},
-        std::vector<glm::mat4>{glm::mat4{ 1.0f }}) };
+        model_drawable_ptrs,
+        model_mats) };
+    scene.add_program(shader_city_prog);
+
+
+    // TODO: Wrap this in a drawing tracer object
+    // TODO: From here
+    /*
+        This object is used to draw both radio station and reflections
+        Object input arguments:
+        - tx_pos, rx_pos: positions of the transmitter and receiver
+        - sig_tracer: a shared pointer to the signal tracer object
+
+
+        Methods:
+        set_program()
+        set_light(light, program)
+    */
+
+    // set drawing for radio stations
+    // ----------------------------------
+    auto station_object_ptr{ std::make_shared<signal_tracer::Cubesphere>(0.25f, 3, true) };
+    std::vector<std::shared_ptr<signal_tracer::Drawable>> station_objects{ };
+    std::vector<glm::mat4> station_model_mats{};
+    for (const auto& pos : { tx_pos, rx_pos }) {
+        station_objects.push_back(station_object_ptr);
+        station_model_mats.push_back(glm::translate(glm::mat4{ 1.0f }, pos));
+    }
 
     auto shader_radio_prog{ std::make_shared<signal_tracer::ShaderProgram>(
         "radio_object",
         radio_prog,
         std::vector<std::shared_ptr<signal_tracer::Light>>{directional_light_ptr},
         std::vector<glm::vec3>{glm::vec3{ 1.0f }},
-        std::vector<std::shared_ptr<signal_tracer::Drawable>>{station_object_ptr, station_object_ptr},
+        station_objects,
         station_model_mats) };
 
+    // drawing reflections
+    //     ----------------------------------
+    std::vector<std::shared_ptr<signal_tracer::Drawable>> lines{};
+    std::vector<glm::mat4> line_model_mats{};
+
+    for (const auto& ref_record : sig_tracer->get_reflection_records()) {
+        lines.push_back(std::make_shared<signal_tracer::Line>(ref_record));
+        line_model_mats.push_back(glm::mat4{ 1.0f });
+    }
     auto shader_line_prog{ std::make_shared<signal_tracer::ShaderProgram>(
         "line",
         line_prog,
@@ -171,9 +160,8 @@ int main(int argc, char* argv []) {
         std::vector<glm::vec3>{glm::vec3{ 1.0f }},
         lines,
         line_model_mats) };
+    // TODO: wrapper to here
 
-
-    scene.add_program(shader_city_prog);
     scene.add_program(shader_radio_prog);
     scene.add_program(shader_line_prog);
 
