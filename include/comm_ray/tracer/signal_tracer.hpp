4241c4c069c1f9a179341693f3be8fa995c8c2c7
hieule <hieu.tg.le@gmail.com>
2023-07-13 17:01:43 +0700

    move path loss calculation to propagation model

--- include/comm_ray/tracer/signal_tracer.hpp
+++ include/comm_ray/tracer/signal_tracer.hpp
@@ -17,17 +17,18 @@
 
 namespace SignalTracer {
 
-    /*
-        Instructions:
+    /**
+    @brief
+    Instructions:
 
-        1 Initilization Part:
-        - Initialize a SignalTracer object with a vector of Mesh objects.
-        - Call the tracing() function to get all reflection records.
-        - Call the init_draw() function to initialize the Line objects for drawing.
+    1 Initilization Part:
+    - Initialize a SignalTracer object with a vector of Mesh objects.
+    - Call the tracing() function to get all reflection records.
+    - Call the init_draw() function to initialize the Line objects for drawing.
 
-        2 Drawing Part:
-        - Call the draw_radio_stations() function to draw the radio stations.
-        - Call the draw() function to draw the Line objects.
+    2 Drawing Part:
+    - Call the draw_radio_stations() function to draw the radio stations.
+    - Call the draw() function to draw the Line objects.
     */
     class SignalTracer {
     public:
@@ -195,27 +196,6 @@ namespace SignalTracer {
                 }
             }
             return is_reflect;
-        }
-
-        /// @brief Calculate the free space loss in dB using the Friss formula
-        /// @param distance Meters
-        /// @param frequency Frequency in Hz
-        /// @return Free space loss in dB
-        float calc_free_space_loss(float distance, float frequency) const {
-            // constant 10*log((4*pi/c)^2) = -147.55221677811664
-            return 20.0f * std::log10(distance) + 20.0f * std::log10(frequency) - 147.55221677811664f;
-        }
-
-        /// @brief Calculate the received power in dBm using the Friss formula
-        /// @param tx_power Power of transmitter in dBm
-        /// @param tx_gain Gain of transmitter in dB
-        /// @param rx_gain Gain of receiver in dB
-        /// @param distance Distance between transmitter and receiver in meters
-        /// @param frequency Frequency in Hz
-        /// @return Received power in dBm
-        float calc_received_power_freespace(float tx_power, float tx_gain, float rx_gain, float distance, float frequency) const {
-            float free_space_loss = calc_free_space_loss(distance, frequency);
-            return tx_power + tx_gain + rx_gain - free_space_loss;
         }
 
         bool is_direct_lighting() const { return m_is_direct_lighting; }
