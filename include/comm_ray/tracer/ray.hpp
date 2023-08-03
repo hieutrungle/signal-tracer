93490ca56441626b481ef9aae8233ec487575a74
hieule <hieu.tg.le@gmail.com>
2023-08-03 15:40:32 +0700

    add 1/direction

--- include/comm_ray/tracer/ray.hpp
+++ include/comm_ray/tracer/ray.hpp
@@ -13,13 +13,18 @@ namespace SignalTracer {
         Ray() = default;
         Ray(const glm::vec3& origin, const glm::vec3& direction)
             : m_origin{ origin }
-            , m_direction{ glm::normalize(direction) } {}
+            , m_direction{ glm::normalize(direction) }
+            , m_inv_direction{ 1.0f / m_direction.x, 1.0f / m_direction.y, 1.0f / m_direction.z } {}
 
         const glm::vec3& get_origin() const { return m_origin; }
         const glm::vec3& get_direction() const { return m_direction; }
+        const glm::vec3& get_inv_direction() const { return m_inv_direction; }
 
         void set_origin(const glm::vec3& origin) { m_origin = origin; }
-        void set_direction(const glm::vec3& direction) { m_direction = glm::normalize(direction); }
+        void set_direction(const glm::vec3& direction) {
+            m_direction = glm::normalize(direction);
+            m_inv_direction = glm::vec3{ 1.0f / m_direction.x, 1.0f / m_direction.y, 1.0f / m_direction.z };
+        }
 
         // negate ray
         Ray operator-() const { return Ray(m_origin, -m_direction); }
@@ -34,6 +39,7 @@ namespace SignalTracer {
     private:
         glm::vec3 m_origin{};
         glm::vec3 m_direction{};
+        glm::vec3 m_inv_direction{};
     };
 
 }
